e360e6a3a5f97fd96e8129fd41334492c003b6d3
hercikmta <hercik@m-ta.cz>
2024-12-22 13:33:49 +0100

    Example for DHT11 and DS11B20

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,21 @@
 #include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 String* parseGET(String queryString);
 
+void Sensosr_DHT11();
 #include <DHT.h>
 #define DHTPIN 1
 #define DHTTYPE DHT11 
 DHT dht(DHTPIN, DHTTYPE);
 
-void Sensosr_DHT11();
+void Sensosr_DS11B20();
+#define ONE_WIRE_BUS 2  // Pin pro připojení datového vodiče senzoru
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -20,10 +28,8 @@ void loop() {
 
   delay(1000);
 
-  // CMD Parser
-  
   // Příklad GET dotazu
-  String exampleQuery = "type=dht11&resolution=0.1";
+  String exampleQuery = "type=dht11&id=0&resolution=0.1";
 
   // Parsování příkladu
   String* result = parseGET(exampleQuery);
@@ -37,13 +43,14 @@ void loop() {
   // Call of specific sensor
   sensorID = result[1].toInt();
 
-  // TESTING //
-  // sensorID = 0;
+  sensorID = 1;
+
   switch (sensorID) {
     case 0: // Sensor: 
       Sensosr_DHT11();
       break;
     case 1: // Sensor:
+      Sensosr_DS11B20();
       break;
     case 2: // Sensor:
       break;
@@ -57,6 +64,14 @@ void loop() {
 
 }
 
+void Sensosr_DS11B20()
+{
+  sensors.requestTemperatures();  // Požádej senzory o teplotu
+  float temperature = sensors.getTempCByIndex(0);  // Čti první senzor
+  Serial.print(F("?type=ds11b20&id=0&temp="));
+  Serial.println(temperature);
+}
+
 void Sensosr_DHT11()
 {
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
@@ -64,20 +79,20 @@ void Sensosr_DHT11()
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
   // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println(F("?type=dht11&id=2&temp=nan&humi=nan"));
-    return;
-  }
+  //float f = dht.readTemperature(true);
+  //if (isnan(h) || isnan(t) || isnan(f)) {
+  //  Serial.println(F("?type=dht11&id=2&temp=nan&humi=nan"));
+    //return;
+  //}
   // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
+  //float hif = dht.computeHeatIndex(f, h);
   // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
+  //float hic = dht.computeHeatIndex(t, h, false);
   
-  Serial.print(F("?type=dht11&id=2&humi="));
+  Serial.print(F("?type=dht11&id=0&humi="));
   Serial.print(h);
   Serial.print(F("&temp="));
-  Serial.print(t);
+  Serial.println(t);
 }
 
 
