f8a078ead5de5706b3e5ec37ba4b7d945aa83da3
SteelMist <jan.rojicek@email.cz>
2025-01-16 17:32:23 +0100

    2 i2c sensory
    asi bude vhodné když je předělám do header jako kolega  a důkladně okomentuji (Sensor_GY_521,Sensor_GY_BMP280)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <Wire.h>
 
 String* parseGET(String queryString);
 
@@ -21,6 +22,30 @@ void Specific_Sensor_AnalogRead(int analogPin, String typeOfSensor,int sensorID)
 
 void Specific_Sensor_DigitalRead(int digitalPin, String typeOfSensor,int sensorID);
 
+
+
+//I2C.............................................
+
+#define SDASCL 16,17
+//GY_521
+void Sensor_GY_521();
+#define GY_521ADDRESS 0x68
+#define GY_521POWER 0x6B
+//GY_BMP280
+void Sensor_GY_BMP280RealValue(int32_t rPres,int32_t rTemp);
+void Sensor_GY_BMP280();
+#define GY_BMP280ADDRESS 0x76
+#define GY_BMP280_POWERMODE 0xF4
+#define GY_BMP280_TMP 0xFA
+#define GY_BMP280_PRESS 0xF7
+//sanpling settings
+#define GY_BMP280_REG_CONFIG 0xF5
+//calibration 
+void Sensor_GY_BMP280CALIB();
+int32_t t_fine;
+int16_t calib_T1, calib_T2, calib_T3;
+int16_t calib_P1, calib_P2, calib_P3, calib_P4, calib_P5, calib_P6, calib_P7, calib_P8, calib_P9;
+//..................................................
 
 void setup() {
   Serial.begin(115200);
@@ -187,4 +212,113 @@ String* parseGET(String queryString) {
   }
 
   return values;
+}
+
+
+void Sensor_GY_521()
+{
+  
+ Wire.beginTransmission(GY_521ADDRESS);
+ Wire.write(GY_521POWER);
+ //set to 0 to turn on | set to 0x40 to sleep
+ Wire.write(0);
+ Wire.endTransmission(true);
+ //getting data
+  Wire.beginTransmission(GY_521ADDRESS);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(GY_521ADDRESS,14,true);
+  int16_t acm_x=Wire.read()<<8 | Wire.read();
+  int16_t acm_y=Wire.read()<<8 | Wire.read();
+  int16_t acm_z=Wire.read()<<8 | Wire.read();
+  float temperatureCelsius=(Wire.read()<<8 | Wire.read())/ 340.0 + 35.00;
+  int16_t gyr_x=Wire.read()<<8 | Wire.read();
+  int16_t gyr_y=Wire.read()<<8 | Wire.read();
+  int16_t gyr_z=Wire.read()<<8 | Wire.read();
+
+ Wire.beginTransmission(GY_521ADDRESS);
+ Wire.write(GY_521POWER);
+ //set to 0 to turn on | set to 0x40 to sleep
+ Wire.write(0x40);
+ Wire.endTransmission(true);
+}
+
+void Sensor_GY_BMP280CALIB(){
+ Wire.beginTransmission(GY_BMP280ADDRESS);
+ Wire.write(0x88);   
+ Wire.endTransmission();
+ Wire.requestFrom(GY_BMP280ADDRESS, 24);
+
+ calib_T1 = Wire.read() | (Wire.read() << 8);
+ calib_T2 = Wire.read() | (Wire.read() << 8);
+ calib_T3 = Wire.read() | (Wire.read() << 8);
+ calib_P1 = Wire.read() | (Wire.read() << 8);
+ calib_P2 = Wire.read() | (Wire.read() << 8);
+ calib_P3 = Wire.read() | (Wire.read() << 8);
+ calib_P4 = Wire.read() | (Wire.read() << 8);
+ calib_P5 = Wire.read() | (Wire.read() << 8);
+ calib_P6 = Wire.read() | (Wire.read() << 8);
+ calib_P7 = Wire.read() | (Wire.read() << 8);
+ calib_P8 = Wire.read() | (Wire.read() << 8);
+ calib_P9 = Wire.read() | (Wire.read() << 8);
+}
+
+void Sensor_GY_BMP280RealValue(int32_t rPres,int32_t rTemp)
+{
+  int32_t var = ((((rTemp >> 3) - ((int32_t)calib_T1 << 1))) * ((int32_t)calib_T2)) >> 11;
+  int32_t var0 = (((((rTemp >> 4) - ((int32_t)calib_T1)) * ((rTemp >> 4) - ((int32_t)calib_T1))) >> 12) * ((int32_t)calib_T3)) >> 14;
+  t_fine = var + var0; // t_fine is an intermediate value for temperature calculation
+
+
+float t = (t_fine * 5 + 128) >> 8;  // t_fine is in 0.01 degrees Celsius
+t /= 100.0; // Convert to °C
+
+   int64_t var1 = ((int64_t)t_fine) - 128000;
+int64_t var2 = var1 * var1 * (int64_t)calib_P6;
+var2 = var2 + ((var1 * (int64_t)calib_P5) << 17);
+var2 = var2 + (((int64_t)calib_P4) << 35);
+var1 = ((var1 * var1 * (int64_t)calib_P3) >> 8) + ((var1 * (int64_t)calib_P2) << 12);
+var1 = (((((int64_t)1) << 47) + var1)) * ((int64_t)calib_P1) >> 33;
+
+
+int64_t p = 1048576 - rPres;  
+p = (((p<<31)-var2)*3125)/var1;
+var1 = (((int64_t)calib_P9) * (p>>13)*(p>>13)) >>25;
+var2= (((int64_t)calib_P8) *p)>>19;
+p = ((p + var1+var2)>>8)+(((int64_t)calib_P7)<<4);
+
+
+// Convert p to hPa (final pressure in hPa)
+p = p / 256.0/100 ;  
+
+ 
+}
+void Sensor_GY_BMP280()
+{
+
+
+ //mode
+ Wire.beginTransmission(GY_BMP280ADDRESS);
+ Wire.write(GY_BMP280_POWERMODE);
+ //defines imaging and power mode
+ Wire.write(10001001);
+ Wire.endTransmission(true);
+ // forced mode mesurment mesurses data and turns to sleep mode after a while
+
+
+
+   Wire.beginTransmission(GY_BMP280ADDRESS);
+  Wire.write(GY_BMP280_REG_CONFIG);
+  Wire.write(1);  // defines filter
+  Wire.endTransmission();
+ //getting data
+ delay(100);
+   Wire.beginTransmission(GY_BMP280ADDRESS);
+  Wire.write(GY_BMP280_PRESS);  // Starting from presure register 
+  Wire.endTransmission();
+  Wire.requestFrom(GY_BMP280ADDRESS, 6);
+  int32_t rawPress = Wire.read() << 12 | Wire.read() << 4 | Wire.read() >> 4;
+  int32_t rawTemp = Wire.read() << 12 | Wire.read() << 4 | Wire.read() >> 4;
+  Sensor_GY_BMP280RealValue(rawPress,rawTemp);
+
 }
