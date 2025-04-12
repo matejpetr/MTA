c2fa0bd128ea70751670fe77eb0c59ad220fa27a
matejpetr <matej.petr.st@vsb.cz>
2025-04-12 13:26:06 +0200

    Updated version

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,32 @@
 #include <Arduino.h>
-
 #include "Parser.hpp"
 
-#include "Senzor_DHT11.hpp"
 #include "Senzor_DS18B20.hpp"
+#include "Senzor_DHT11.hpp"
+//#include "Ahall.hpp"    
 #include "Senzor_GY_521.hpp"
+//#include "HCSR04.hpp"   
+//#include "HCSR501.hpp"
 #include "Senzor_BMP280.hpp"
+//include "Antc.hpp"
+//include "PIresistance.hpp"
+//include "Joystick.hpp"
+//include "HallLin.hpp"
+//include "MQ135.hpp"
+//include "IndPNP.hpp"
+//include "IndNPN.hpp"
+//include "AMoisture.hpp"
+//include "TTP223.hpp"
+//include "AJSR04M.hpp"
+//include "GP2Y0A21YK0F.hpp"
+//include "Encoder.hpp"
+//include "HS0038DB.hpp"
+//include "MicSmall.hpp"
+//include "MicBig.hpp"
+//include "Heartbeat.hpp"
 #include "Senzor_AnalogRead.hpp"
 #include "Senzor_DigitalRead.hpp"
+
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
@@ -50,24 +69,127 @@ void loop() {
   pinNo    = result[3].toInt(); // pinNo
 
   switch (sensorID) {
-    case 0: // Sensor: 
-      Sensor_DHT11(oneWire);
-      break;
-    case 1: // Sensor:
+    case 0: // DS18B20
       Sensor_DS18B20(sensors);
       break;
-    case 3: // Digital Hall Sensor:
-      Specific_Sensor_DigitalRead(pinNo,type,sensorID);
+    case 1: // DHT11
+      Sensor_DHT11(oneWire);
       break;
-    case 4: // Analog Hall Sensor:
-      Specific_Sensor_AnalogRead(pinNo,type,sensorID);
+    case 3: // DHall Sensor:
+      Sensor_DigitalRead(pinNo);
       break;
-    case 5: // BMP280
-      Sensor_GY_BMP280();
+    case 4: // AHall Sensor:
+      //Sensor_Ahall();
       break;
-    case 6: // GZ_521
+    case 5: // GY521
       Sensor_GY_521();
       break;
+    case 6: // FC51
+      //Sensor_FC51();
+      break;
+    case 7: // HCSR04
+      //Sensor_HCSR04();
+      break;
+    case 8: // HCSR501
+      //Sensor_HCSR501();
+      break;
+    case 9: // KW113Z
+      //Sensor_KW113Z();
+      break;
+    case 10: // BMP280
+      Sensor_GY_BMP280();
+      break;
+    case 11: // TCS34725
+      //Sensor_TCS34725();
+      break;
+    case 12: // VL53L0X
+      //Sensor_VL53L0X();
+      break;
+    case 13: // InfraredR
+      //Sensor_InfraredR();
+      break;
+    case 14: // InfraredE
+      //Sensor_InfraredE();
+      break;
+    case 15: // Dntc
+      //Sensor_Dntc();
+      break;
+    case 16: // Antc
+      //Sensor_Antc();
+      break;
+    case 17: // PIresistance
+      //Sensor_PIresistance();
+      break;
+    case 18: // Joystick
+      //Sensor_Joystick();
+      break;
+    case 19: // HallLin
+      //Sensor_HallLin();
+      break;
+    case 20: // MQ135
+      //Sensor_MQ135();
+      break;
+    case 21: //IndPNP
+      //Sensor_IndPNP();
+      break;
+    case 22: // IndNPN
+      //Sensor_IndNPN();
+      break;
+    case 23: // Dmoisture
+      //Sensor_Dmoisture();
+      break;
+    case 24: // AMoisture
+      //Sensor_AMoisture();
+      break;
+    case 25: // TTP223
+      //Sensor_TTP223();
+      break;
+    case 26: // AJSR04M
+      //Sensor_AJSR04M();
+      break;
+    case 27: // GP2Y0A21YK0F
+      //Sensor_GP2Y0A21YK0F();
+      break;
+    case 28: // RCWL0516
+      //Sensor_RCWL0516();
+      break;
+    case 29: // Encoder
+      //Sensor_Encoder();
+      break;
+    case 30: // HS0038DB
+      //Sensor_HS0038DB();
+      break;
+    case 31: // TCRT5000
+      //Sensor_TCRT5000();
+      break;
+    case 32: // IRflame
+      //Sensor_IRflame();
+      break;
+    case 33: // REED
+      //Sensor_REED();
+      break;
+    case 34: // MicSmall
+      //Sensor_MicSmall();
+      break;
+    case 35: // MicBig
+      //Sensor_MicBig();
+      break;
+    case 36: // MetalTouch
+      //Sensor_MetalTouch();
+      break;
+    case 37: // Heartbeat
+      //Sensor_Heartbeat();
+      break;
+    case 38: // Btn
+      //Sensor_Btn();
+      break;
+    case 39: // TiltSwitch
+      //Sensor_TiltSwitch();
+      break;
+    case 40: // Dvibration
+      //Sensor_Dvibration();
+      break;
+      
     case 100: // Universal Diagital Input Read; type=DigitalRead&id=100&pinNo=x;
       Sensor_DigitalRead(pinNo);
       break;
@@ -78,6 +200,6 @@ void loop() {
       Serial.println("?id=" + String(sensorID) + "&message=NotSupported");
       sensorID = -1;
       break;
-  }
+  
 
 }
