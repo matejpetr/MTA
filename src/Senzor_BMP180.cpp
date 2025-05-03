b8f40b0ecb4bc31a98d9f81da074416ff8700651
matejpetr <matej.petr.st@vsb.cz>
2025-05-03 23:37:18 +0200

    updt

--- src/Senzor_BMP180.cpp
+++ src/Senzor_BMP180.cpp
@@ -2,9 +2,8 @@
 #include <Adafruit_BMP085.h>
 #include "Senzor_BMP180.hpp"
 
-#define BMP085_ADDRESS 0x77
+#define BMP085_ADDRESS 0x77 //defaultní I2C adresa pro ESP32
 
-// Vytvoříme globální objekty
 TwoWire myWire(0);  // vlastní I2C instance
 Adafruit_BMP085 bmp180;
 
@@ -12,23 +11,20 @@ bool Sensor_BMP180_Init(int SDA, int SCL) {
   myWire.begin(SDA, SCL);
 
   if (!bmp180.begin(BMP085_ADDRESS, &myWire)) {
-    Serial.println("BMP180 se nepodařilo inicializovat.");
     return false;
   }
-
-  Serial.println("BMP180 inicializován.");
+  
   return true;
 }
 
 void Sensor_BMP180() {
   int korekce = 32;
 
-  long raw = bmp180.readPressure();
-  float corrected = (raw + korekce * 100) / 100.0;
-  float altitude = bmp180.readAltitude(raw + korekce * 100);
+  float p = (bmp180.readPressure() + korekce * 100) / 100.0;
+  float a = bmp180.readAltitude(bmp180.readPressure() + korekce * 100);
 
   Serial.print(F("?type=BMP180&id=14&press="));
-  Serial.print(corrected);
+  Serial.print(p);
   Serial.print(F("&altitude="));
-  Serial.println(altitude);
+  Serial.println(a);
 }
