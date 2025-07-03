2196b7a8d0720ef9b9d9657df2056de8c7246184
Vojcin <vojtech.kucera.st@vsb.cz>
2025-07-03 21:49:42 +0200

    Testik

--- MTA_tester/Form1.cs
+++ MTA_tester/Form1.cs
@@ -60,7 +60,7 @@ namespace GUI
         private void InitializeChart()
         {
             chart1.Series.Clear();
-            //MTA ! MTA! MTA! MTA! MTA! 
+            //MTA ! MTA! MTA! MTA! MTA! MTA!  
 
             // Přidání výchozí série (nepoužívá se přímo, ale inicializuje graf)
             Series series = new Series("measuring")
