7a65f8f21ec3b0c583dfbaf0a498a3f5902c0bef
Vojcin <vojtech.kucera.st@vsb.cz>
2025-07-05 13:34:44 +0200

    Simulator

---
+++ Simulator/Simulator1/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Simulator1
+{
+    partial class MTA_Simulator
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+       
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MTA_Simulator));
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.pnlStatus = new System.Windows.Forms.Panel();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.comBox = new System.Windows.Forms.ComboBox();
+            this.sensorBox = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblComPort = new System.Windows.Forms.Label();
+            this.lblSensor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(30, 30);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(120, 40);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Připojit";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(180, 40);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 16);
+            this.lblStatus.TabIndex = 1;
+            // 
+            // pnlStatus
+            // 
+            this.pnlStatus.BackColor = System.Drawing.Color.Transparent;
+            this.pnlStatus.Location = new System.Drawing.Point(406, 34);
+            this.pnlStatus.Name = "pnlStatus";
+            this.pnlStatus.Size = new System.Drawing.Size(30, 30);
+            this.pnlStatus.TabIndex = 2;
+            this.pnlStatus.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlStatus_Paint);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(30, 263);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(406, 175);
+            this.textBox.TabIndex = 3;
+            // 
+            // comBox
+            // 
+            this.comBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comBox.FormattingEnabled = true;
+            this.comBox.Location = new System.Drawing.Point(91, 90);
+            this.comBox.Name = "comBox";
+            this.comBox.Size = new System.Drawing.Size(70, 24);
+            this.comBox.TabIndex = 5;
+            // 
+            // sensorBox
+            // 
+            this.sensorBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.sensorBox.FormattingEnabled = true;
+            this.sensorBox.ItemHeight = 16;
+            this.sensorBox.Location = new System.Drawing.Point(307, 90);
+            this.sensorBox.Name = "sensorBox";
+            this.sensorBox.Size = new System.Drawing.Size(129, 24);
+            this.sensorBox.TabIndex = 7;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(49, 178);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(131, 38);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Spustit";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(305, 178);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(131, 38);
+            this.btnStop.TabIndex = 9;
+            this.btnStop.Text = "Zastavit";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblComPort
+            // 
+            this.lblComPort.AutoSize = true;
+            this.lblComPort.Location = new System.Drawing.Point(19, 93);
+            this.lblComPort.Name = "lblComPort";
+            this.lblComPort.Size = new System.Drawing.Size(66, 16);
+            this.lblComPort.TabIndex = 4;
+            this.lblComPort.Text = "COM port:";
+            // 
+            // lblSensor
+            // 
+            this.lblSensor.AutoSize = true;
+            this.lblSensor.Location = new System.Drawing.Point(215, 98);
+            this.lblSensor.Name = "lblSensor";
+            this.lblSensor.Size = new System.Drawing.Size(52, 16);
+            this.lblSensor.TabIndex = 6;
+            this.lblSensor.Text = "Senzor:";
+            // 
+            // MTA_Simulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 443);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.sensorBox);
+            this.Controls.Add(this.lblSensor);
+            this.Controls.Add(this.comBox);
+            this.Controls.Add(this.lblComPort);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.pnlStatus);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnConnect);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MTA_Simulator";
+            this.Text = "MTA_Simulator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Panel pnlStatus;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ComboBox comBox;
+        private System.Windows.Forms.ComboBox sensorBox;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblComPort;
+        private System.Windows.Forms.Label lblSensor;
+    }
+}
