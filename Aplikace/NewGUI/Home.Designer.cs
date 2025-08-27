0b6f2c5654037f81642991ab1c863c1d968ba69e
Vojta-Kucera <vojtech.kucera.st@vsb.cz>
2025-08-27 22:24:40 +0200

    V0.4-poppingText_under_buttons

--- Aplikace/NewGUI/Home.Designer.cs
+++ Aplikace/NewGUI/Home.Designer.cs
@@ -28,41 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Sensor_panel = new System.Windows.Forms.Panel();
             this.Simulator_panel = new System.Windows.Forms.Panel();
+            this.Simulator_button = new System.Windows.Forms.Button();
+            this.Sensor_panel = new System.Windows.Forms.Panel();
+            this.Sensor_button = new System.Windows.Forms.Button();
             this.Aktuatory_panel = new System.Windows.Forms.Panel();
             this.Aktuatory_button = new System.Windows.Forms.Button();
-            this.Simulator_button = new System.Windows.Forms.Button();
-            this.Sensor_button = new System.Windows.Forms.Button();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.Sensor_panel.SuspendLayout();
             this.Simulator_panel.SuspendLayout();
+            this.Sensor_panel.SuspendLayout();
             this.Aktuatory_panel.SuspendLayout();
-            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // Sensor_panel
-            // 
-            this.Sensor_panel.Controls.Add(this.Sensor_button);
-            this.Sensor_panel.Location = new System.Drawing.Point(182, 3);
-            this.Sensor_panel.Name = "Sensor_panel";
-            this.Sensor_panel.Size = new System.Drawing.Size(173, 130);
-            this.Sensor_panel.TabIndex = 2;
             // 
             // Simulator_panel
             // 
             this.Simulator_panel.Controls.Add(this.Simulator_button);
-            this.Simulator_panel.Location = new System.Drawing.Point(361, 3);
+            this.Simulator_panel.Location = new System.Drawing.Point(411, 3);
             this.Simulator_panel.Name = "Simulator_panel";
-            this.Simulator_panel.Size = new System.Drawing.Size(173, 130);
+            this.Simulator_panel.Size = new System.Drawing.Size(173, 336);
             this.Simulator_panel.TabIndex = 3;
+            // 
+            // Simulator_button
+            // 
+            this.Simulator_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(218)))), ((int)(((byte)(215)))));
+            this.Simulator_button.Font = new System.Drawing.Font("Bahnschrift", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Simulator_button.Image = global::NewGUI.Properties.Resources.half_brain_mini3;
+            this.Simulator_button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Simulator_button.Location = new System.Drawing.Point(-6, -7);
+            this.Simulator_button.Name = "Simulator_button";
+            this.Simulator_button.Size = new System.Drawing.Size(187, 150);
+            this.Simulator_button.TabIndex = 1;
+            this.Simulator_button.Text = "Simulator";
+            this.Simulator_button.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Simulator_button.UseMnemonic = false;
+            this.Simulator_button.UseVisualStyleBackColor = false;
+            this.Simulator_button.Click += new System.EventHandler(this.Simulator_button_Click);
+            // 
+            // Sensor_panel
+            // 
+            this.Sensor_panel.Controls.Add(this.Sensor_button);
+            this.Sensor_panel.Location = new System.Drawing.Point(232, 3);
+            this.Sensor_panel.Name = "Sensor_panel";
+            this.Sensor_panel.Size = new System.Drawing.Size(173, 336);
+            this.Sensor_panel.TabIndex = 2;
+            // 
+            // Sensor_button
+            // 
+            this.Sensor_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(218)))), ((int)(((byte)(215)))));
+            this.Sensor_button.Font = new System.Drawing.Font("Bahnschrift", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Sensor_button.Image = global::NewGUI.Properties.Resources.half_brain_mini3;
+            this.Sensor_button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Sensor_button.Location = new System.Drawing.Point(-6, -7);
+            this.Sensor_button.Name = "Sensor_button";
+            this.Sensor_button.Size = new System.Drawing.Size(187, 150);
+            this.Sensor_button.TabIndex = 1;
+            this.Sensor_button.Text = "Senzory";
+            this.Sensor_button.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Sensor_button.UseMnemonic = false;
+            this.Sensor_button.UseVisualStyleBackColor = false;
+            this.Sensor_button.Click += new System.EventHandler(this.Sensor_button_Click);
             // 
             // Aktuatory_panel
             // 
             this.Aktuatory_panel.Controls.Add(this.Aktuatory_button);
-            this.Aktuatory_panel.Location = new System.Drawing.Point(3, 3);
+            this.Aktuatory_panel.Location = new System.Drawing.Point(53, 3);
             this.Aktuatory_panel.Name = "Aktuatory_panel";
-            this.Aktuatory_panel.Size = new System.Drawing.Size(173, 130);
+            this.Aktuatory_panel.Size = new System.Drawing.Size(173, 336);
             this.Aktuatory_panel.TabIndex = 4;
             // 
             // Aktuatory_button
@@ -81,72 +111,30 @@
             this.Aktuatory_button.UseVisualStyleBackColor = false;
             this.Aktuatory_button.Click += new System.EventHandler(this.Aktuatory_button_Click);
             // 
-            // Simulator_button
-            // 
-            this.Simulator_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(218)))), ((int)(((byte)(215)))));
-            this.Simulator_button.Font = new System.Drawing.Font("Bahnschrift", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.Simulator_button.Image = global::NewGUI.Properties.Resources.half_brain_mini3;
-            this.Simulator_button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Simulator_button.Location = new System.Drawing.Point(-6, -7);
-            this.Simulator_button.Name = "Simulator_button";
-            this.Simulator_button.Size = new System.Drawing.Size(187, 150);
-            this.Simulator_button.TabIndex = 1;
-            this.Simulator_button.Text = "Simulator";
-            this.Simulator_button.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.Simulator_button.UseMnemonic = false;
-            this.Simulator_button.UseVisualStyleBackColor = false;
-            this.Simulator_button.Click += new System.EventHandler(this.Simulator_button_Click);
-            // 
-            // Sensor_button
-            // 
-            this.Sensor_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(218)))), ((int)(((byte)(215)))));
-            this.Sensor_button.Font = new System.Drawing.Font("Bahnschrift", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.Sensor_button.Image = global::NewGUI.Properties.Resources.half_brain_mini3;
-            this.Sensor_button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Sensor_button.Location = new System.Drawing.Point(-6, -7);
-            this.Sensor_button.Name = "Sensor_button";
-            this.Sensor_button.Size = new System.Drawing.Size(187, 150);
-            this.Sensor_button.TabIndex = 1;
-            this.Sensor_button.Text = "Senzory";
-            this.Sensor_button.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.Sensor_button.UseMnemonic = false;
-            this.Sensor_button.UseVisualStyleBackColor = false;
-            this.Sensor_button.Click += new System.EventHandler(this.Sensor_button_Click);
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Controls.Add(this.Aktuatory_panel);
-            this.flowLayoutPanel1.Controls.Add(this.Sensor_panel);
-            this.flowLayoutPanel1.Controls.Add(this.Simulator_panel);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(538, 136);
-            this.flowLayoutPanel1.TabIndex = 5;
-            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.Simulator_panel);
+            this.Controls.Add(this.Sensor_panel);
+            this.Controls.Add(this.Aktuatory_panel);
             this.Name = "Home";
-            this.Size = new System.Drawing.Size(538, 381);
-            this.Sensor_panel.ResumeLayout(false);
+            this.Size = new System.Drawing.Size(714, 462);
             this.Simulator_panel.ResumeLayout(false);
+            this.Sensor_panel.ResumeLayout(false);
             this.Aktuatory_panel.ResumeLayout(false);
-            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.Panel Sensor_panel;
-        private System.Windows.Forms.Button Sensor_button;
+
         private System.Windows.Forms.Panel Simulator_panel;
         private System.Windows.Forms.Button Simulator_button;
+        private System.Windows.Forms.Panel Sensor_panel;
+        private System.Windows.Forms.Button Sensor_button;
         private System.Windows.Forms.Panel Aktuatory_panel;
         private System.Windows.Forms.Button Aktuatory_button;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
--- Aplikace/NewGUI/Home.cs
+++ Aplikace/NewGUI/Home.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace NewGUI
 {
@@ -14,130 +15,52 @@ namespace NewGUI
     {
         private Form1 _rodic;
 
-        private readonly Dictionary<Button, (int progress, int target)> _wipe =
-         new Dictionary<Button, (int progress, int target)>();
-        private Timer animTimer;
-        private int speedPx = 25;
-        private Color _hoverColor = Color.FromArgb(243, 95, 0);
-        private readonly Color _textNormal = Color.White;
         public Home(Form1 rodic)
         {
             InitializeComponent();
             _rodic = rodic;
 
-            animTimer = new Timer { Interval = 15 };
-            animTimer.Tick += AnimTimer_Tick;
-            Animation(Aktuatory_button, Properties.Resources.half_brain_mini4, hostPanel: Aktuatory_panel);
-            Animation(Sensor_button, Properties.Resources.half_brain_mini4, hostPanel: Sensor_panel);
-            Animation(Simulator_button, Properties.Resources.half_brain_mini4, hostPanel: Simulator_panel);
+            // Předpoklad: máš tři host panely (A … B … C) v Designeru:
+            // Aktuatory_panel, Sensor_panel, Simulator_panel
+            // Vyčisti je a vlož HoverTile s Dock=Fill
+            AddTile(Aktuatory_panel,
+                title: "Aktuátory",
+                normal: Properties.Resources.half_brain_mini3,
+                hover:  Properties.Resources.half_brain_mini4,   // můžeš dát přebarvenou verzi
+                detail: "Ovládání aktivních prvků přes seriovou komunikaci",
+                onActivate: (_, __) => _rodic.NahraditObsah(new Aktuatory(_rodic)));
+
+            AddTile(Sensor_panel,
+                title: "Senzory",
+                normal: Properties.Resources.half_brain_mini3,
+                hover:  Properties.Resources.half_brain_mini4,
+                detail: "Měření pomocí senzorů přes seriovou komunikaci",
+                onActivate: (_, __) => _rodic.NahraditObsah(new Senzory(_rodic)));
+
+            AddTile(Simulator_panel,
+                title: "Simulator",
+                normal: Properties.Resources.half_brain_mini3,
+                hover:  Properties.Resources.half_brain_mini4,
+                detail: "Virtuální prostředí pro testování senzorů a aktuátorů.",
+                onActivate: (_, __) => _rodic.NahraditObsah(new Simulator(_rodic)));
         }
 
-
-        private void Animation(Button btn, Image OrangeImage, Panel hostPanel = null)
+        private static void AddTile(Panel host, string title, Image normal, Image hover, string detail, EventHandler onActivate)
         {
-            if (!_wipe.ContainsKey(btn))
-                _wipe.Add(btn, (0, 0));
+            host.Controls.Clear();
 
-            btn.UseMnemonic = false; // aby & nebylo bráno jako zkratka
-
-            btn.Paint += (s, e) =>
+            var tile = new Tlacitka
             {
-                var g = e.Graphics;
-                var rect = btn.ClientRectangle;
-                var (progress, _) = _wipe[btn];
-                int h = Math.Min(progress, rect.Height);
-
-                // 1) Oranžový pruh zdola nahoru
-                Rectangle overlayRect = Rectangle.Empty;
-                if (progress > 0)
-                {
-                    overlayRect = new Rectangle(0, rect.Height - h, rect.Width, h);
-                    using (var hoverBrush = new SolidBrush(_hoverColor))
-                        g.FillRectangle(hoverBrush, overlayRect);
-                }
-                if (btn.Image != null)
-                {
-                    Image imgToDraw = btn.Image;
-
-                    // pokud je tlačítko z 50% a víc "hoverované", použij přebarvenou verzi
-                    if (progress > rect.Width / 5)
-                    {
-                        imgToDraw = OrangeImage;
-                    }
-
-                    // spočítáme pozici obrázku přibližně na střed (neřešíme přesně Align kvůli jednoduchosti)
-                    int x = 5;
-                    int y = (rect.Height - imgToDraw.Height) / 2;
-
-                    g.DrawImage(imgToDraw, x, y, imgToDraw.Width, imgToDraw.Height);
-                }
-                // 2) Zarovnání textu (TopCenter) – malý padding shora (+4)
-                Rectangle textRect = new Rectangle(
-                    rect.Left,
-                    rect.Top + 4,
-                    rect.Width,
-                    rect.Height
-                );
-                var tff = TextFormatFlags.Top | TextFormatFlags.HorizontalCenter | TextFormatFlags.EndEllipsis;
-
-                // 3) Nejprve celý text černě (viditelný mimo overlay)
-                TextRenderer.DrawText(g, btn.Text, btn.Font, textRect, Color.Black, tff);
-
-                // 4) Stejný text modře, ale jen uvnitř overlay (přebarvení při najetí)
-                if (!overlayRect.IsEmpty)
-                {
-                    g.SetClip(overlayRect, System.Drawing.Drawing2D.CombineMode.Replace);
-                    TextRenderer.DrawText(g, btn.Text, btn.Font, textRect, _textNormal, tff); // _textNormal = RGB(5,92,169)
-                    g.ResetClip();
-                }
+                Dock = DockStyle.Fill,
+                Title = title,
+                NormalImage = normal,
+                HoverImage = hover,
+                DetailText = detail,
+                ExpandedHeight = 150 // výšku můžeš doladit
             };
+            tile.Activated += onActivate;
 
-            btn.MouseEnter += (_, __) => SetTarget(btn, btn.Height);
-            btn.MouseLeave += (_, __) => SetTarget(btn, 0);
-
-            if (hostPanel != null)
-            {
-                hostPanel.MouseEnter += (_, __) => SetTarget(btn, btn.Height);
-                hostPanel.MouseLeave += (_, __) => SetTarget(btn, 0);
-            }
-        }
-
-
-        private void SetTarget(Button btn, int target)
-        {
-            if (!_wipe.ContainsKey(btn)) return;
-            var (progress, _) = _wipe[btn];
-            _wipe[btn] = (progress, target);
-            if (!animTimer.Enabled) animTimer.Start();
-        }
-        private void AnimTimer_Tick(object sender, EventArgs e)
-        {
-            bool anyMoving = false;
-
-            // kopie klíčů, abychom mohli měnit slovník bezpečně
-            var keys = _wipe.Keys.ToList();
-
-            foreach (var btn in keys)
-            {
-                var (progress, target) = _wipe[btn];
-
-                if (progress < target)
-                {
-                    progress = Math.Min(target, progress + speedPx);
-                    anyMoving = true;
-                }
-                else if (progress > target)
-                {
-                    progress = Math.Max(target, progress - speedPx);
-                    anyMoving = true;
-                }
-
-                _wipe[btn] = (progress, target);
-                btn.Invalidate();
-            }
-
-            if (!anyMoving)
-                animTimer.Stop();
+            host.Controls.Add(tile);
         }
 
         private void Simulator_button_Click(object sender, EventArgs e)
